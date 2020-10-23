4a8c1e5f0473a1d1c94c7faf918a26e624835ca7
daniyal <mdaniyal9@bitbucket.org>
2020-10-23 10:45:43 +0500

    worked on basics of hilt

---
+++ app/src/main/java/com/example/hilttestapp/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.hilttestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.FragmentScoped
+import javax.inject.Inject
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    // field injection
+    @Inject
+    lateinit var someClass: SomeClass
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println(someClass.doAThing1())
+        println(someClass.doAThing2())
+    }
+}
+@AndroidEntryPoint
+class MyFragment: Fragment(){
+    @Inject
+    lateinit var someClass: SomeClass
+}
+
+@ActivityScoped
+class SomeClass
+@Inject
+constructor(
+    @Impl1 private val someInterfaceImpl1: SomeInterface,
+    @Impl2 private val someInterfaceImpl2: SomeInterface
+){
+    fun doAThing1(): String{
+        return "Look I got: ${someInterfaceImpl1.getAThing()}"
+    }
+
+    fun doAThing2(): String{
+        return "Look I got: ${someInterfaceImpl2.getAThing()}"
+    }
+}
+
+
+class SomeInterfaceImpl1
+@Inject
+constructor(): SomeInterface{
+    override fun getAThing() : String{
+        return "A Thing1"
+    }
+}
+
+class SomeInterfaceImpl2
+@Inject
+constructor(): SomeInterface{
+    override fun getAThing() : String{
+        return "A Thing2"
+    }
+}
+
+interface SomeInterface{
+    fun getAThing(): String
+}
+@InstallIn(ApplicationComponent::class)
+@Module
+class MyModule{
+
+    @Impl1
+    @Singleton
+    @Provides
+    fun provideSomeInterface1():SomeInterface{
+        return SomeInterfaceImpl1()
+    }
+
+    @Impl2
+    @Singleton
+    @Provides
+    fun provideSomeInterface2():SomeInterface{
+        return SomeInterfaceImpl2()
+    }
+}
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Impl1
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Impl2
